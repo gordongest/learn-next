fd2dd78beb147e7f26dc2ccc362d934b67b4d41f
Gordon Gest <gordon.gest@gmail.com>
2022-09-16 13:22:50 -0600

    accidentally renamed wrong page; re-renaming moveis to contact and about to movies

---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css'
+
+const ContactPage = () => {
+    return (
+        <h3 className={styles.card}>don't contact us ever</h3>
+    )
+}
+
+export default ContactPage;
